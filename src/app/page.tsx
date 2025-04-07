346a84ead389c7ddcf9e0ddf043b29193d6e3d46
Syarif Miftahudin <miftahudin.syarif@gmail.com>
2025-04-07 12:43:12 +0700

    Refactor Home component layout for improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,39 +3,28 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-gray-100">
-      {/* Hero Section */}
-      <div className="min-h-screen">
-        <div className="mx-auto px-4 h-full flex items-center justify-center">
-          <div className="grid md:grid-cols-2 gap-100 items-center">
-            <div className="space-y-6">
-              <div className="w-12 h-12 bg-[#E74C3C] rounded-full mb-8"></div>
-              <h1 className="text-6xl font-bold">
-                I'm Syarif
-                <br />
-                Miftahdin
-              </h1>
-              <p className="text-gray-600 text-lg max-w-lg">
-                a student in Purwadhika
-              </p>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6 gap-30">
+      <div>
+        <h1 className="text-6xl font-bold leading-tight">
+          I'm Syarif
+          <br />
+          Miftahudin
+        </h1>
+        <p className="text-gray-600 text-lg mb-10">a student in Purwadhika</p>
 
-              <button className="bg-black text-white px-8 py-3 rounded-none hover:bg-gray-800 transition">
-                READ MORE
-              </button>
-            </div>
+        <button className="bg-black text-white px-8 py-3 rounded-none hover:bg-gray-800 ">
+          READ MORE
+        </button>
+      </div>
 
-            <div className="relative h-[600px] w-full flex items-center justify-center">
-              <Image
-                src="/rex.png"
-                alt="Rex Profile"
-                fill
-                style={{ objectFit: "contain" }}
-                priority
-                className="p-4"
-              />
-            </div>
-          </div>
-        </div>
+      <div className="relative w-[300px] h-[300px] rounded-full overflow-hidden shadow-lg">
+        <Image
+          src="/rex.png"
+          alt="Rex Profile"
+          width={300}
+          height={300}
+          className="p-4"
+        />
       </div>
     </div>
   );
